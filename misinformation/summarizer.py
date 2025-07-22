fafaf61b85297dd8f6b689537059421e3d59f3a7
ckish3 <ckish1@gmail.com>
2025-07-22 16:13:02 -0700

    add rouge evaluation

--- misinformation/summarizer.py
+++ misinformation/summarizer.py
@@ -1,6 +1,7 @@
 """This module provides a class to summarize a piece of text"""
 
 from transformers import pipeline
+from rouge_score import rouge_scorer
 
 
 class Summarizer:
@@ -18,3 +19,18 @@ class Summarizer:
             str: The summarization
         """
         return self.summarizer(text, max_length=max_length)[0]["summary_text"]
+
+    def evaluate_rouge(self, text: str, reference: str) -> dict:
+        """
+        Evaluates the ROUGE score of a piece of text
+        Args:
+            text (str): The text to evaluate
+            reference (str): The reference text
+
+        Returns:
+            dict: The ROUGE scores as a dictionary, where the keys are the metric names
+                and the values are a Score object
+        """
+        scorer = rouge_scorer.RougeScorer(['rouge1', 'rouge2', 'rougeL'], use_stemmer=True)
+        scores = scorer.score(text, reference)
+        return scores
